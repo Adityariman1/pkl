d66a723394b453ccc759394df3d5d1b921bb93fd
Adityariman1 <adityariman96@gmail.com>
2022-01-13 16:44:37 +0700

    api

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('adminlte::page')
+
+@section('title', 'Toko Buku')
+
+@section('content_header')
+
+
+
+@endsection
+
+@section('content')
+
+Selamat Datang Di Halaman Admin
+
+@endsection
+
+@section('css')
+
+@endsection
+
+@section('js')
+
+@endsection
